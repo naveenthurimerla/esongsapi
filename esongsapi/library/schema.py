486b73f71f60e7e9e4f978f22fb2a462e4c8f49f
Naveen Thurimerla <naveenreddythurimerla@gmail.com>
2020-06-26 23:01:26 +0530

    add filter by pk

--- esongsapi/library/schema.py
+++ esongsapi/library/schema.py
@@ -41,15 +41,68 @@ class LyricType(DjangoObjectType):
 
 
 class Query(object):
+    
+    language = graphene.Field(LanguageType, id=graphene.Int())
+    artist = graphene.Field(ArtistType, id=graphene.Int())
+    album = graphene.Field(AlbumType, id=graphene.Int())
+    track = graphene.Field(TrackType, id=graphene.Int())
+    genre = graphene.Field(GenreType, id=graphene.Int())
+    company = graphene.Field(CompanyType, id=graphene.Int())
+    lyric = graphene.Field(LyricType, id=graphene.Int())
+
+
     all_languages = graphene.List(LanguageType)
     all_artists = graphene.List(ArtistType)
     all_albums = graphene.List(AlbumType)
     all_tracks = graphene.List(TrackType)
-
     all_genres = graphene.List(GenreType)
     all_companies = graphene.List(CompanyType)
     all_lyrics = graphene.List(LyricType)
 
+    def resolve_language(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Language.objects.get(pk=id)
+
+        return None
+    def resolve_artist(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Artist.objects.get(pk=id)
+
+        return None
+    def resolve_album(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Album.objects.get(pk=id)
+
+        return None        
+    def resolve_track(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Track.objects.get(pk=id)
+
+        return None
+    def resolve_genre(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Genre.objects.get(pk=id)
+
+        return None        
+
+    def resolve_lyric(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Lyric.objects.get(pk=id)
+
+        return None 
+    
     def resolve_all_languages(self, info, **kwargs):
         return Language.objects.all()
 
